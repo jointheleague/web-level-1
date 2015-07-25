e2256a918e108f69ed604a35e9488a9e00b7c0eb
Trey Hunner <trey@treyhunner.com>
2015-07-25 14:11:27 -0700

    Add random name material

---
+++ random-name/js/names.js
@@ -0,0 +1,29 @@
+// What's in a Name?
+
+// 6. Create a function that we will call when our button is clicked
+
+// 7. Begin our function by creating two arrays, one for first names, one for last names
+//  --Pick a theme (i.e. pirates, robots, monsters) and fill your arrays with 5-10 names
+
+// 8. Now use getElementById to select our p tag and assign it to a variable
+
+// 9. Select a random element from the first name array
+//  --Remember to assign the output of each expression to a variable
+//    --Generate a random number
+//    --Multiply that number by the length of the array
+//    --Use Math.floor to convert the number to an integer
+//  --Use the number you've generated to select an element from the array
+
+// 10. Repeat the steps in 9 to select an element from the last name array.
+//  BONUS: Instead of repeating the steps, create a function that accepts
+//          an array as its argument, and outputs a randomly select element
+
+// 11. Now combine the first name and last name into a single string
+//  --Combining strings is called concatenation
+//  --You can concatenate strings in Javascript by adding them together with a +
+//  --Example: newString = firstString + secondString + thirdString;
+
+// 12. Take the combined string and insert it into the innerText of the name tag
+
+// 13. Once you've demonstrated that your script is working, go to your stylesheet
+//      and style your page according to the theme you picked for the names.
