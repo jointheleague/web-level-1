0fe3bde27e2e836813dc0d11bc4467df31392e84
Trey Hunner <trey@treyhunner.com>
2015-08-08 14:02:19 -0700

    Add Scheming and Plotting exercise

---
+++ scheming-and-plotting/js/plot.js
@@ -0,0 +1,20 @@
+/*
+  12. Create a function called plotScheme
+
+  13. Use jQuery to select the body element and assign it to a variable
+    - We select elements with jQuery the same way we select them in CSS
+      HINT: What are the three ways we can select an element (or elements) in CSS?
+    - Use the $ function to make your selection: e.g. $("div")
+    - Unlike CSS, we need to quote our selector.
+
+  14. Use jQuery to find the value of the selected radio button
+    - use the CSS selector: input[name='color']:checked
+    - assign the selected element to a variable
+
+  15. Get the value of that element by calling the .val function of the variable
+      - Assign this value to a variable
+
+  15. Use the value you assigned to set the background of the body element
+    - use the jQuery function .css('background-color',VARIABLENAME)
+
+*/
